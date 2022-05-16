46cd91492ab78ee0f3ffe047467c45a08a2990bc
Unnamed_DEV <a22yo22io@gmail.com>
2022-05-16 17:57:25 +0200

    Comments
    Recepe folder

--- DownTownFactory/Assets/Scripts/RezepteManager/Rezepte.cs
+++ DownTownFactory/Assets/Scripts/RezepteManager/Rezepte.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 
 public class Rezepte : MonoBehaviour
 {
+    //referanence to some Scripts
     public MoneyManager moneyManager;
-    
+
+    //referances to the craft buttons
     [Header("0 = AxtStick, 1 = AxtHead, 2 = Axt, 3 = Platine")]
     public GameObject[] craftButtons;
+
+    //collection of recipes buttons in the crafafter
     public GameObject[] craftButtonsItems;
+
+    //thes are the buttons to buy the recipes
     public GameObject[] buyButtons;
 
+    //buy AxtStick recipes
     public void AxtStick()
     {
+        //if money is higher than 100
         if (moneyManager.money >= 100)
         {
+            //devede 100 frome the money
             moneyManager.DevedeMoney(100);
+            //set the button in the spawner to true
             craftButtons[0].SetActive(true);
+            //set the item the spawner can pickup now to true in the crafter
             craftButtonsItems[0].SetActive(true);
+            //disable the buy butten for the recipe
             buyButtons[0].SetActive(false);
         }
     }
@@ -27,9 +39,13 @@ public class Rezepte : MonoBehaviour
     {
         if (moneyManager.money >= 100)
         {
+            //devede 100 frome the money
             moneyManager.DevedeMoney(100);
+            //set the button in the spawner to true
             craftButtons[1].SetActive(true);
+            //set the item the spawner can pickup now to true in the crafter
             craftButtonsItems[1].SetActive(true);
+            //disable the buy butten for the recipe
             buyButtons[1].SetActive(false);
         }
     }
@@ -38,9 +54,13 @@ public class Rezepte : MonoBehaviour
     {
         if (moneyManager.money >= 500)
         {
+            //devede 100 frome the money
             moneyManager.DevedeMoney(500);
+            //set the button in the spawner to true
             craftButtons[2].SetActive(true);
+            //set the item the spawner can pickup now to true in the crafter
             craftButtonsItems[2].SetActive(true);
+            //disable the buy butten for the recipe
             buyButtons[2].SetActive(false);
         }
     }
@@ -49,9 +69,13 @@ public class Rezepte : MonoBehaviour
     {
         if (moneyManager.money >= 15000)
         {
+            //devede 100 frome the money
             moneyManager.DevedeMoney(15000);
+            //set the button in the spawner to true
             craftButtons[3].SetActive(true);
+            //set the item the spawner can pickup now to true in the crafter
             craftButtonsItems[3].SetActive(true);
+            //disable the buy butten for the recipe
             buyButtons[3].SetActive(false);
         }
     }
@@ -60,9 +84,13 @@ public class Rezepte : MonoBehaviour
     {
         if (moneyManager.money >= 50000)
         {
+            //devede 100 frome the money
             moneyManager.DevedeMoney(50000);
+            //set the button in the spawner to true
             craftButtons[4].SetActive(true);
+            //set the item the spawner can pickup now to true in the crafter
             craftButtonsItems[4].SetActive(true);
+            //disable the buy butten for the recipe
             buyButtons[4].SetActive(false);
         }
     }
